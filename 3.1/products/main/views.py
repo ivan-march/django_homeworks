40f6032da0e68dfeae951bef7ed7cfcec1671612
Ivan March <ivanumarch@yandex.ru>
2025-05-16 15:08:35 +0300

    Solution for task drf

--- 3.1/products/main/views.py
+++ 3.1/products/main/views.py
@@ -2,31 +2,38 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
 
+from main.models import MARK_CHOICES, Product, Review
 from main.serializers import ReviewSerializer, ProductListSerializer, ProductDetailsSerializer
 
 
 @api_view(['GET'])
 def products_list_view(request):
-    """реализуйте получение всех товаров из БД
-    реализуйте сериализацию полученных данных
-    отдайте отсериализованные данные в Response"""
-    pass
+    products = Product.objects.all()
+    ser = ProductListSerializer(products, many=True)
+    return Response(ser.data)
 
 
 class ProductDetailsView(APIView):
     def get(self, request, product_id):
-        """реализуйте получение товара по id, если его нет, то выдайте 404
-        реализуйте сериализацию полученных данных
-        отдайте отсериализованные данные в Response"""
-        pass
+        product = get_object_or_404(Product, id=product_id)
+        ser = ProductDetailsSerializer(product)
+        return Response(ser.data)
 
 
-# доп задание:
 class ProductFilteredReviews(APIView):
     def get(self, request, product_id):
-        """обработайте значение параметра mark и
-        реализуйте получение отзывов по конкретному товару с определённой оценкой
-        реализуйте сериализацию полученных данных
-        отдайте отсериализованные данные в Response"""
-        pass
+        get_object_or_404(Product, id=product_id)
+        mark = request.GET.get('mark')
+        reviews = Review.objects.filter(product_id=product_id)
+
+        if mark:
+            try:
+                mark = int(mark)
+            except ValueError:
+                return Response({"error": "Параметр 'mark' должен быть числом"}, status=400)
+            reviews = reviews.filter(product_id=product_id, mark=mark)
+
+        ser = ReviewSerializer(reviews, many=True)
+        return Response(ser.data)
